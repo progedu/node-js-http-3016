3317beb9d29f7b00ed686cc3b113e46950cff106
K Sugimoto <36184621+7ma7X@users.noreply.github.com>
2019-10-16 16:15:52 +0900

    add sushi

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const server = http.createServer((req, res) => {
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
+      } else if (req.url === '/enquetes/sushi-pizza') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: '寿司',
+          secondItem: 'ピザ'
+        }));
       }
       res.end();
       break;
